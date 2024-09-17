fd54d0840ec06b91f94d4b775e364e04edf7cfb8
RytasV <rytis.valatkevicius@mif.stud.vu.lt>
2024-09-17 23:10:06 +0300

    Create Type Page with typing container and two buttons

--- Typeracer/wwwroot/js/site.js
+++ Typeracer/wwwroot/js/site.js
@@ -1,6 +1,10 @@
 ﻿function startGame(){
     var audio = new Audio('/sounds/click.mp3');
     audio.play();
+
+    audio.onended = function() {
+        location.href = '/Home/Type';
+    };
     
     console.log("Žaidimas pradėtas");
 }

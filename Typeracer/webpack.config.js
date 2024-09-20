032ba789e200e79b925476c883b4188ae98ebed1
Povilas Uckus <povilas.uckus@mif.stud.vu.lt>
2024-09-20 11:52:02 +0300

    Set up Webpack for React bundling

--- Typeracer/webpack.config.js
+++ Typeracer/webpack.config.js
@@ -1,34 +1,25 @@
 ﻿const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',  // React entry point
+    entry: './src/index.js',  // Source file
     output: {
-        path: path.resolve(__dirname, 'wwwroot/js'),
-        filename: 'bundle.js',  // Output file
+        path: path.resolve(__dirname, 'wwwroot/js'),  // Where to save the generated file
+        filename: 'bundle.js',  // Generated file name
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,  // Treat all .js files
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'babel-loader',  // Using Babel to transpile
                 },
             },
             {
-                test: /\.css$/,  // Files with .css extension
+                test: /\.css$/,  // Treat all .css files
                 use: ['style-loader', 'css-loader'],  // Loaders to use
             },
         ],
     },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
-    ],
+    mode: 'development',  // Development mode
 };

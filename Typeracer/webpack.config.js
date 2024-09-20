5c79572c0344225a1bfd4818b71f1832f34d0116
Povilas Uckus <povilas.uckus@mif.stud.vu.lt>
2024-09-20 10:05:28 +0300

    Configured Webpack and Babel for React integration

---
+++ Typeracer/webpack.config.js
@@ -0,0 +1,34 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',  // React entry point
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/js'),
+        filename: 'bundle.js',  // Output file
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,  // Files with .css extension
+                use: ['style-loader', 'css-loader'],  // Loaders to use
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+    ],
+};

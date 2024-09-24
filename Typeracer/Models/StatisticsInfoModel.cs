47698afff93b2c0d614a822d7fa2fe81b770c8b9
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-09-24 22:31:29 +0300

    Comments for the StatisticsInfoModel.cs

--- Typeracer/Models/StatisticsInfoModel.cs
+++ Typeracer/Models/StatisticsInfoModel.cs
@@ -2,16 +2,16 @@ namespace Typeracer.Models;
 
 public class StatisticsInfoModel
 {
-    private TimeSpan CompletionTime { get; set; } // time that it took to finish the entire paragraph (or finish typing)
-    private DateTime LocalStartTime { get; set; } // start date and time of the game
-    private DateTime LocalFinishTime { get; set; } // end date and time of the game
+    public TimeSpan CompletionTime { get; set; } // time that it took to finish the entire paragraph (or finish typing)
+    public DateTime LocalStartTime { get; set; } // start date and time of the game
+    public DateTime LocalFinishTime { get; set; } // end date and time of the game
     
-    private int TotalAmountOfWords { get; set; } // amount of words in the paragraph
-    private int TotalAmountOfCharacters { get; set; } // amount of characters in the paragraph;
-    private int TypedAmountOfWords { get; set; }
-    private int TypedAmountOfCharacters { get; set; }
-    private int NumberOfWrongfulCharacters { get; set; }
-    public List<TypingData> TypingData { get; set; } = new List<TypingData>();
+    public int TotalAmountOfWords { get; set; } // amount of words in the paragraph
+    public int TotalAmountOfCharacters { get; set; } // amount of characters in the paragraph;
+    public int TypedAmountOfWords { get; set; }
+    public int TypedAmountOfCharacters { get; set; }
+    public int NumberOfWrongfulCharacters { get; set; }
+    public List<TypingData> TypingData { get; set; } = new List<TypingData>(); // storing all data about the typed characters
     
 }
 

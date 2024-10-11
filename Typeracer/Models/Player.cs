78a08c54a249dfa5147b62b67bfee15b94d924dd
Elvinas Švilpa <elvinas.svilpa@mif.stud.vu.lt>
2024-10-11 10:36:33 +0300

    Implemented initial Player class

---
+++ Typeracer/Models/Player.cs
@@ -0,0 +1,12 @@
+namespace Typeracer.Models;
+
+// values of a player can change, so struct is used instead of record
+public struct Player
+{
+    public readonly Guid PlayerID { get; }
+    public string Username { get; set; }
+    public double AverageWPM { get; set; }
+    public double AverageAccuracy { get; set; }
+    public double BestWPM { get; set; }
+    
+}
